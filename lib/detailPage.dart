b53afe85055f0aa6e1162a382bff2ca57e5c1fa0
anggaraswn <109957618+anggaraswn@users.noreply.github.com>
2022-09-20 15:47:41 +0700

    Add files via upload

---
+++ lib/detailPage.dart
@@ -0,0 +1,60 @@
+import 'package:agfood/secondTab.dart';
+import 'package:agfood/util/ChangeThemeToggle.dart';
+import 'package:agfood/util/firstTab.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DetailPage extends StatelessWidget {
+  int itemId;
+  String itemTitle;
+  String itemDescription;
+  String itemImage;
+  String username;
+
+  DetailPage(
+      {super.key,
+      required this.itemId,
+      required this.itemTitle,
+      required this.itemDescription,
+      required this.username,
+      required this.itemImage});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          title: const Text('AGFood'),
+          backgroundColor: const Color(0xff54BAB9),
+          elevation: 0,
+        ),
+        body: SafeArea(
+            child: Column(
+          children: [
+            const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.info_outline)),
+                Tab(icon: Icon(Icons.insert_comment_outlined))
+              ],
+              labelColor: Colors.blueGrey,
+            ),
+            Expanded(
+                child: TabBarView(children: [
+              FirstTab(
+                itemId: itemId,
+                itemTitle: itemTitle,
+                itemDescription: itemDescription,
+                itemImage: itemImage,
+                username: username,
+              ),
+              SecondTab(postId: itemId)
+            ]))
+          ],
+        )),
+      ),
+    );
+  }
+}
---
+++ lib/item.dart
@@ -0,0 +1,12 @@
+class Items {
+  int id;
+  String title;
+  String image;
+  String description;
+
+  Items(
+      {required this.id,
+      required this.title,
+      required this.image,
+      required this.description});
+}
